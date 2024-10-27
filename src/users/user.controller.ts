54a1901c3c7ba8055f3cdd12f486cb741928bf75
romua1d <shmidtelson@gmail.com>
2024-10-27 23:05:24 +0300

    chore: Update doc

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -14,6 +14,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -29,7 +30,7 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @ApiOperation({ summary: 'Register a user' })
-  @ApiOkResponse({ description: 'User registered', type: UserDto })
+  @ApiCreatedResponse({ description: 'User registered', type: UserDto })
   @ApiNotFoundResponse({ description: 'User not found' })
   @ApiBadRequestResponse({ description: 'Invalid data' })
   @Post('register')
